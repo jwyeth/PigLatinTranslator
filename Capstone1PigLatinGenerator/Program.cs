e49f66e52a1c47cd46f584856def279b358cb9f1
jwyeth <jwyeth@hotmail.com>
2021-01-22 17:45:08 -0500

    Cleaned up and added method

--- Capstone1PigLatinGenerator/Program.cs
+++ Capstone1PigLatinGenerator/Program.cs
@@ -6,69 +6,49 @@ namespace Capstone1PigLatinGenerator
     {
         static void Main(string[] args)
         {
+            string goAgain = "y";
+            string input;
+            string output;
+
             Console.WriteLine("Welcome to the Pig Latin Translator!");
 
-            string goAgain = "y";
             while (goAgain == "y")
             {
                 Console.Write("Please enter a word to be translated: ");
-                string input = Console.ReadLine();
+                input = Console.ReadLine();
                 string lowerInput = input.ToLower();
-                string output;
-
-
                 string firstLetter = lowerInput.Substring(0, 1);
-                string restOfSentence = lowerInput.Substring(1, lowerInput.Length - 1);
-                //int isVowel = firstLetter.IndexOf(vowels);
 
-                //if (lowerInput.Length == 1 && firstLetter == "a" || firstLetter == "i" || firstLetter == "e" || firstLetter == "o" || firstLetter == "u"))
-
-
-
-
-                if (lowerInput.Length <= 1)
-                {
-                    Console.WriteLine("Please enter a word to translate.");
-                }
-                else if (firstLetter == "a" || firstLetter == "i" || firstLetter == "e" || firstLetter == "o" || firstLetter == "u")
+                if (firstLetter == "a" || firstLetter == "i" || firstLetter == "e" || firstLetter == "o" || firstLetter == "u")
                 {
                     output = lowerInput + "way";
                     Console.WriteLine(output);
                 }
-                else if (lowerInput.Length <= 2)
-                {
-                    output = restOfSentence + firstLetter + "ay";
-                    Console.WriteLine(output);
-                }
-                else
-                {
-                    int index = lowerInput.IndexOfAny("aeiou".ToCharArray());
-                    Console.WriteLine(index);
-                    string front = lowerInput.Substring(0, index);
-                    string back = lowerInput.Substring(index);
-                    //Console.WriteLine(front);
-                    //Console.WriteLine(back);
-                    //Console.WriteLine(index);
-                    output = back + front + "ay";
-                    Console.WriteLine(output);
-                }
+                else output = TranslateToPigLatin(lowerInput);
 
+                Console.WriteLine(output);
                 Console.Write("Would you like to translate another word? (y/n) ");
-                goAgain = Console.ReadLine();
 
-                if (goAgain != "y")
-                {
-                    break;
-                }
-
+                goAgain = Console.ReadLine().ToLower();
             }
         }
 
-       /*public static string TranslateToPigLatin()
+        public static string TranslateToPigLatin(string lowerInput)
         {
-                            string firstLetter = lowerInput.Substring(0, 1);
-                string restOfSentence = lowerInput.Substring(1, lowerInput.Length - 1);
+         
 
-        }*/
+            
+            int index = lowerInput.IndexOfAny("aeiou".ToCharArray());
+
+            if (index == -1)
+                return "There doesn't seem to be any vowels.";
+            else
+            {
+                string front = lowerInput.Substring(0, index);
+                string back = lowerInput.Substring(index);
+                string output = back + front + "ay";
+                return output;
+            }
+        }
     }
 }
